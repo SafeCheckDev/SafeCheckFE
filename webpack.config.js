33ae8f073e703c5ddba4d8b64523e032e583454d
Tara Ojo <tara.ojo@futurelearn.com>
2018-01-06 15:27:50 +0000

    Fix eslint `missing-trailing-comma` violation
    The eslint rules we've setup prefers traliing commas. This commit adds
    the missing ones.

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ const path = require('path');
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, 'app', 'index.html'),
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 module.exports = {
   entry: path.join(__dirname, 'app', 'index.js'),
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     loaders: [
@@ -31,15 +31,15 @@ module.exports = {
         test: /\.(ico)$/,
         loader: 'url-loader',
       },
-    ]
+    ],
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'build.js'
+    filename: 'build.js',
   },
   context: path.join(__dirname, 'app'),
   plugins: [HTMLWebpackPluginConfig,
     new CopyWebpackPlugin([
-      { from: 'images/favicon.ico' }
+      { from: 'images/favicon.ico' },
     ])],
 };
